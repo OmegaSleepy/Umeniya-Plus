7a38f4f71894632c7f0c80ed315fcb97c465ff93
martin <martingeorgiev2307@gmail.com>
2026-01-15 22:25:04 +0200

    Migrated old Tag names into the new Tag names, kept the methods for future use

--- src/main/java/omega/sleepy/Migrate.java
+++ src/main/java/omega/sleepy/Migrate.java
@@ -1,16 +1,59 @@
 package omega.sleepy;
 
 import omega.sleepy.util.Log;
+import org.jetbrains.annotations.NotNull;
 
 import java.sql.*;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Migrate {
     record Blog(int id, String title, String tag, String excerpt, String content) {}
 
     public static void main(String[] args) throws SQLException {
+//        migrateDB();
+        migrateLanguage();
+    }
+
+    private static void migrateLanguage() throws SQLException {
+        Connection db = DriverManager.getConnection("jdbc:sqlite:main.db");
+        Map<String, String> langMap = getStringStringMap();
+
+        for (Map.Entry<String, String> entry : langMap.entrySet()) {
+            String newName = entry.getKey();
+            String oldName = entry.getValue();
+
+            String sql = "UPDATE blogs set tag = ? where tag = ?";
+            try (PreparedStatement ps = db.prepareStatement(sql)) {
+                ps.setString(1, newName);
+                ps.setString(2, oldName);
+                ps.executeUpdate();
+            }
+        }
+
+        db.close();
+
+    }
+
+    private static @NotNull Map<String, String> getStringStringMap() {
+        var newList = List.of("Математика", "Наука", "Биология", "Химия", "Физика", "Английски език", "История",
+                "География", "Изкуство", "Музика", "Компютърни науки", "Икономика", "Философия",
+                "Литература", "Няма", "Влякакви");
+        var oldList = List.of("Mathematics", "Science", "Biology", "Chemistry", "Physics", "English", "History",
+                "Geography", "Art", "Music", "Computer Science", "Economics", "Philosophy",
+                "Literature", "None", "Any");
+
+        Map<String, String> langMap =  new HashMap<>();
+        for (int i = 0; i < newList.size(); i++) {
+            langMap.put(newList.get(i), oldList.get(i));
+        }
+        return langMap;
+    }
+
+    private static void migrateDB() throws SQLException {
         Connection old_laptop = DriverManager.getConnection("jdbc:sqlite:server.db");
         Connection old_laptop2 = DriverManager.getConnection("jdbc:sqlite:old_laptop.db");
         Connection old_oc = DriverManager.getConnection("jdbc:sqlite:old_pc.db");
@@ -53,6 +96,5 @@ public class Migrate {
             pstmt.close();
         }
         newOne.close();
-
     }
 }
