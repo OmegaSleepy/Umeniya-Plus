ea64b08b332ed2847fc723d674b189d91272c4d8
martin <martingeorgiev2307@gmail.com>
2026-01-15 10:23:40 +0200

    Forgot to add .gitignore

--- target/classes/sql/blogSchema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE if not exists blogs
-(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
-    tag TEXT NOT NULL,
-    excerpt TEXT NOT NULL,
-    content TEXT NOT NULL
-)
--- target/classes/sql/userSchema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE if not exists USERS
-(
-    username TEXT PRIMARY KEY,
-    password_hash TEXT,
-    role TEXT NOT NULL,
-    registrated_at TIMESTAMP,
-    last_login TIMESTAMP
-)
