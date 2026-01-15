3ef83677f13e9e42f33becf5f766aeb0345fdcce
martin <martingeorgiev2307@gmail.com>
2026-01-15 10:24:13 +0200

    Simple public route

--- src/main/java/omega/sleepy/routes/PublicRoutes.java
+++ src/main/java/omega/sleepy/routes/PublicRoutes.java
@@ -1,7 +1,20 @@
 package omega.sleepy.routes;
 
+
+import omega.sleepy.util.Log;
+import omega.sleepy.util.MediaType;
+
+import static spark.Spark.*;
+
 public class PublicRoutes {
     public static void init(){
+        Log.info("Public routes initializing...");
 
+        get("/", (request, response) -> {
+            response.type(MediaType.TXT.getValue());
+            return "HELLO WORLD";
+        });
+
+        Log.info("All public rouses initialized");
     }
 }
