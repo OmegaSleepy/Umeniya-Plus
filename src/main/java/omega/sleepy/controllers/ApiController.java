6db12e0f640975b2cb59a0fd26cf60a3e68f3d28
martin <martingeorgiev2307@gmail.com>
2026-01-15 11:20:09 +0200

    fix ApiRoutes

--- src/main/java/omega/sleepy/controllers/ApiController.java
+++ src/main/java/omega/sleepy/controllers/ApiController.java
@@ -1,4 +1,50 @@
 package omega.sleepy.controllers;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import omega.sleepy.dao.BlogDao;
+import omega.sleepy.routes.ApiRoutes;
+import omega.sleepy.util.Log;
+import spark.Request;
+import spark.Response;
+import spark.utils.IOUtils;
+
 public class ApiController {
+
+    private static JsonParser jsonParser;
+
+    public static String getStyleSheet(Response response) {
+        try (var inputStream = ApiRoutes.class.getResourceAsStream("/public/css/umeniyaStyleSheet.css")) {
+            if (inputStream == null) {
+                response.status(404);
+                return "/* CSS file not found in classpath */";
+            }
+            return IOUtils.toString(inputStream);
+        } catch (Exception e) {
+            response.status(500);
+            return "/* Server Error loading CSS */";
+        }
+    }
+
+    public static String saveBlog(Request request) {
+        JsonObject body = jsonParser.parse(request.body()).getAsJsonObject();
+
+        String title = body.get("title").getAsString();
+        String category = body.get("category").getAsString();
+        String excerpt = body.get("excerpt").getAsString();
+        String content = body.get("content").getAsString();
+
+        if(category.equalsIgnoreCase("Any")) category = "None";
+
+        //data class
+        Blog blog = new Blog(0, title,category,excerpt,content);
+
+        //in /validation
+        if(!isValidBlog(blog)) return "{\"status\":\"not ok\"}";
+
+        Log.info(blog.toString());
+        BlogDao.addBlog(blog);
+
+        return "{\"status\":\"ok\"}";
+    }
 }
--- src/main/java/omega/sleepy/routes/ApiRoutes.java
+++ src/main/java/omega/sleepy/routes/ApiRoutes.java
@@ -1,7 +1,36 @@
 package omega.sleepy.routes;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import omega.sleepy.dao.BlogDao;
+import omega.sleepy.util.Log;
+import omega.sleepy.util.MediaType;
+import spark.Request;
+import spark.Response;
+import spark.utils.IOUtils;
+
+import static omega.sleepy.controllers.ApiController.getStyleSheet;
+import static omega.sleepy.controllers.ApiController.saveBlog;
+import static spark.Spark.*;
+
 public class ApiRoutes {
     public static void init(){
 
+        Gson gson = new Gson();
+
+        get("api/style", (request, response) -> {
+            response.type("text/css");
+
+            return getStyleSheet(response);
+        });
+
+        get("/api/blog/tags", (request, response) -> BlogDao.getCategories(), gson::toJson);
+
+        post("/api/blog/content", (request, response) -> saveBlog(request), gson::toJson);
+
     }
+
+
+
+
 }
