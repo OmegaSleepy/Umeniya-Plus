c5f59f1fd2313716f1cda3afe00c25ac67a9dac5
martin <martingeorgiev2307@gmail.com>
2026-01-17 19:09:57 +0200

    Removed unnecessary comments and todo

--- src/main/java/omega/sleepy/routes/ApiRoutes.java
+++ src/main/java/omega/sleepy/routes/ApiRoutes.java
@@ -13,14 +13,11 @@ public class ApiRoutes {
 
 
         path("/api/blog", () ->{
-            //TODO deprecated, use /api/filer/post/
-//            get("basic_view", ApiController::getFilteredView);
 
             post("/create", ApiController::createBlog);
-            //TODO deprecated, use /api/filter/post/
-//            get("short_blogs", ApiController::getFilteredView);
 
             get("/get-filtered-view/", ApiController::getFilteredView);
+
             get("/tags", ApiController::getCategories);
 
             get("/:id", ApiController::getBlogContentsById);
