28f60ffc125dd02f9ef33c23fd6997661330c935
martin <martingeorgiev2307@gmail.com>
2026-01-18 17:03:28 +0200

    Fixed blogs saving with current time not date and time

--- src/main/java/omega/sleepy/services/BlogService.java
+++ src/main/java/omega/sleepy/services/BlogService.java
@@ -4,6 +4,7 @@ import omega.sleepy.dao.BlogDao;
 import omega.sleepy.data.Blog;
 import omega.sleepy.util.BlogFilter;
 
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +17,7 @@ public class BlogService {
     public static boolean saveBlog(String title, String category, String excerpt, String content) {
         if(category.equalsIgnoreCase("Any")) category = "None";
 
-        Blog blog = new Blog(0, title,category,excerpt,content, "None", LocalTime.now().toString());
+        Blog blog = new Blog(0, title,category,excerpt,content, "None", LocalDateTime.now().toString());
 
         if(isValidBlog(blog)) {
             BlogDao.addBlog(blog);
