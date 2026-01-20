4624a59d0a54bf2964a55c01d682b16f5d5d4cf7
martin <martingeorgiev2307@gmail.com>
2026-01-20 09:19:57 +0200

    Added empty controllers for login and sign-up and a simple example for how a password hashing and checking should work

--- src/main/java/omega/sleepy/controllers/AuthController.java
+++ src/main/java/omega/sleepy/controllers/AuthController.java
@@ -1,6 +1,30 @@
 package omega.sleepy.controllers;
 
-//TODO org.mindrot.jbcrypt.BCrypt
+import org.mindrot.jbcrypt.BCrypt;
+import spark.Request;
+import spark.Response;
 
 public class AuthController {
+    public static void main(String[] args) {
+        String hash = BCrypt.hashpw("i-love-birds",BCrypt.gensalt(10));
+
+        if(BCrypt.checkpw("i-love-pigeons", hash)) {
+            System.out.println("Password is valid");
+        } else {
+            System.out.println("Password is invalid");
+        }
+
+        System.out.println(hash);
+    }
+
+    public static String logIn(Request request, Response response){
+        System.out.println(request.queryParams("password"));
+        return "ok";
+    }
+
+    public static String signUp(Request request, Response response){
+        System.out.println(request.queryParams("password"));
+        return "ok";
+    }
+
 }
