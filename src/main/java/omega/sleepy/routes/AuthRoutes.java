591edce15b4135aa6d9bbe73f6926e2508a0433b
martin <martingeorgiev2307@gmail.com>
2026-01-20 09:19:15 +0200

    Added api endpoints for sign-up and log-in that reference AuthController.java

--- src/main/java/omega/sleepy/routes/AuthRoutes.java
+++ src/main/java/omega/sleepy/routes/AuthRoutes.java
@@ -1,7 +1,17 @@
 package omega.sleepy.routes;
 
+import omega.sleepy.controllers.AuthController;
+
+import static spark.Spark.*;
+
 public class AuthRoutes {
     public static void init(){
+        path("/auth/credentials/", () ->{
+            post("/log-in", AuthController::logIn);
+
+            post("/sign-up", AuthController::signUp);
+        });
+
 
     }
 }
