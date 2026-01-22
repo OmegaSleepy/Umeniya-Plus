5ad8a6f7bf974a29084e1befbbb4dada585cd563
martin <martingeorgiev2307@gmail.com>
2026-01-22 08:56:40 +0200

    Add a better skeleton to AuthService with hints on what to implement

--- src/main/java/omega/sleepy/services/AuthService.java
+++ src/main/java/omega/sleepy/services/AuthService.java
@@ -5,18 +5,24 @@ import org.mindrot.jbcrypt.BCrypt;
 
 public class AuthService {
 	
-	public static boolean checkValidity(String password, String username){
+	public static boolean checkValidity(String username, String plainTextPassword){
 		String passwordHash = UserDao.getPasswordHashFromUsername(username);
-		return BCrypt.checkpw(password, passwordHash);
-		
+		return BCrypt.checkpw(plainTextPassword, passwordHash);
 	}
 
-	public static boolean changePassword(String password, String username){
-
+	public static boolean changePassword(String username, String newPasswordPT, String oldPasswordPT){
+		//check if new password is not old password
+		//check if old password is valid
+		//change password for the user
+		return true;
 	}
 
-	public static boolean deleteProfile(String username){
-
+	public static boolean deleteProfile(String username, String password){
+		// check if the user password is right, if so delete the profile
+		return true;
 	}
-	
+
+	public static String hashPassword(String plainTextPassword){
+		return BCrypt.hashpw(plainTextPassword, BCrypt.gensalt());
+	}
 }
