3f6637f05fd1cb6e3a31cb6beb597e714dcfe8d5
martin <martingeorgiev2307@gmail.com>
2026-01-22 11:20:25 +0200

    Iterated on the skeleton of AuthController.java

--- src/main/java/omega/sleepy/controllers/AuthController.java
+++ src/main/java/omega/sleepy/controllers/AuthController.java
@@ -1,30 +1,35 @@
 package omega.sleepy.controllers;
 
-import org.mindrot.jbcrypt.BCrypt;
+import omega.sleepy.exceptions.MalformedPassword;
+import omega.sleepy.services.AuthService;
+import omega.sleepy.util.Log;
 import spark.Request;
 import spark.Response;
 
+import static omega.sleepy.services.AuthService.createUser;
+
 public class AuthController {
-    public static void main(String[] args) {
-        String hash = BCrypt.hashpw("i-love-birds",BCrypt.gensalt(10));
-
-        if(BCrypt.checkpw("i-love-pigeons", hash)) {
-            System.out.println("Password is valid");
-        } else {
-            System.out.println("Password is invalid");
-        }
-
-        System.out.println(hash);
-    }
 
     public static String logIn(Request request, Response response){
-        System.out.println(request.queryParams("password"));
-        return "ok";
+        String password = request.queryParams("password");
+        String username = request.queryParams("username");
+
+        createUser(username, password);
+
+        return "";
     }
 
     public static String signUp(Request request, Response response){
-        System.out.println(request.queryParams("password"));
-        return "ok";
-    }
+        String password = request.queryParams("password");
+        String username = request.queryParams("username");
 
+        try{
+            createUser(username, password);
+        } catch (MalformedPassword e) {
+            response.status(400);
+            return e.getMessage();
+        }
+
+        return "Valid SignUp";
+    }
 }
