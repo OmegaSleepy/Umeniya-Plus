a5eaf0d96b0640da3d0129e7ff985011acb81ead
martin <martingeorgiev2307@gmail.com>
2026-01-27 09:12:32 +0200

    Improved session handling by adding helper methods to UserDao (contains, deleteOld, usernameFromToken)

--- src/main/java/omega/sleepy/dao/UserDao.java
+++ src/main/java/omega/sleepy/dao/UserDao.java
@@ -8,15 +8,17 @@ import omega.sleepy.util.PermittingLevel;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.Instant;
 import java.time.LocalDateTime;
 
 public class UserDao {
-    public static String getPasswordHashFromUsername(String username){
+    public static String getPasswordHashFromUsername(String username) {
         String sql = "SELECT password_hash from users where username = ?";
 
         try (Connection connection = Database.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
             preparedStatement.setString(1, username);
 
@@ -26,10 +28,10 @@ public class UserDao {
         }
     }
 
-    public static void createUser(String username, String passwordHash){
+    public static void createUser(String username, String passwordHash) {
         String sql = "INSERT into users values(?, ?, ?, ?, ?)";
         try (Connection connection = Database.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, username);
             preparedStatement.setString(2, passwordHash);
             preparedStatement.setString(3, PermittingLevel.USER.toString());
@@ -42,10 +44,10 @@ public class UserDao {
         }
     }
 
-    public static void deleteUser(String username){
+    public static void deleteUser(String username) {
         String sql = "DELETE from users where username = ?";
         try (Connection connection = Database.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, username);
             preparedStatement.execute();
 
@@ -55,10 +57,10 @@ public class UserDao {
 
     }
 
-    public static void changePassword(String username, String newPasswordHash){
+    public static void changePassword(String username, String newPasswordHash) {
         String sql = "UPDATE users set password_hash = ? where username = ?";
         try (Connection connection = Database.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql))   {
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
             preparedStatement.setString(1, newPasswordHash);
             preparedStatement.setString(2, username);
@@ -70,10 +72,10 @@ public class UserDao {
         }
     }
 
-    public static void changeUserPrivalages(String username, PermittingLevel permittingLevel){
+    public static void changeUserPrivalages(String username, PermittingLevel permittingLevel) {
         String sql = "UPDATE users set permittion_level = ? where username = ?";
         try (Connection connection = Database.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql))   {
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
             preparedStatement.setString(1, permittingLevel.toString());
             preparedStatement.setString(2, username);
@@ -85,10 +87,10 @@ public class UserDao {
         }
     }
 
-    public static void addSession(String token, String username, long expiration){
+    public static void addSession(String token, String username, long expiration) {
         String sql = "INSERT into sessions (token, username, expires_at) values (?, ?, ?)";
         try (Connection connection = Database.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, token);
             preparedStatement.setString(2, username);
             preparedStatement.setLong(3, expiration);
@@ -103,13 +105,51 @@ public class UserDao {
     public static void removeSession(String token) {
         String sql = "DELETE from sessions where token = ?";
         try (Connection connection = Database.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, token);
             preparedStatement.execute();
             Log.info("Removed session with UUID " + token);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
     }
-}
+
+    public static boolean containsToken(String token) {
+        String sql = "SELECT * from sessions where token = ?";
+        try (Connection connection = Database.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, token);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                return resultSet.getString("token").equals(token);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void deleteOldTokens() {
+        String sql = "DELETE from sessions where expires_at < ?";
+        try (Connection connection = Database.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setLong(1, Instant.now().getEpochSecond());
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String usernameFromToken(String token) {
+        String sql = "SELECT username from sessions where token = ?";
+        try (Connection connection = Database.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, token);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                return resultSet.getString("username");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    }
