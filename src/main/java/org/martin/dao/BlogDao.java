6fb22b545e307461182d1a543ed4b0cca569c933
martin <martingeorgiev2307@gmail.com>
2026-02-22 21:48:39 +0200

    Total Likes by author

--- src/main/java/org/martin/dao/BlogDao.java
+++ src/main/java/org/martin/dao/BlogDao.java
@@ -321,4 +321,19 @@ public class BlogDao {
             return -3;
         }
     }
+
+    public static int totalLikesByAuthor(String author) {
+        String sql = "SELECT sum(likes) FROM blogs WHERE creator_username = ?";
+        try (Connection conn = getConnection();
+             PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+            preparedStatement.setString(1, author);
+            try (ResultSet rs = preparedStatement.executeQuery()) {
+                rs.next();
+                return rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            Log.error(e.getMessage());
+            return -3;
+        }
+    }
 }
