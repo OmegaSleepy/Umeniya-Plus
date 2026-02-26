4d98f38638a61339f941d4dc2e1dae65d96f59a4
martin <martingeorgiev2307@gmail.com>
2026-02-26 23:42:13 +0200

    Added more tests

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -10,35 +10,33 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class HTTPTest {
-
+public class ApiTest {
     @BeforeAll
     void setup() {
         Main.main(null);
         Spark.awaitInitialization();
     }
 
-    @Test
-    void loadFrontPage(){
-        test("", 200, "Homepage should return 200 Ok");
+    @AfterAll
+    void teardown() {
+        Spark.stop();
     }
 
     @Test
-    void restrictedPage(){
-        test("dashboard", 302, "Dashboard should be restricted");
+    void canEdit(){
+        test("api/blog/can-edit/1", 404, "Should be 404, no cookie");
     }
 
     @Test
-    void fourOFour(){
+    void categories(){
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:4567/3ughe9igb1i3hbe,"))
+                .uri(URI.create("http://localhost:4567/"+"api/blog/tags"))
                 .build();
 
         HttpResponse<String> response = null;
@@ -48,8 +46,26 @@ public class HTTPTest {
             throw new RuntimeException(e);
         }
 
-        assertTrue(response.headers().map().containsValue(List.of("http://localhost:4567/404")), "Random should return 302");
+        assertEquals("{\"Математика\":\"#DCEBFF\",\"Наука\":\"#D9F4F1\",\"Биология\":\"#E3F6E8\",\"Химия\":\"#EFE3FF\",\"Физика\":\"#E1F0FF\",\"Английски език\":\"#FFE3E3\",\"История\":\"#F5EAD6\",\"География\":\"#EEF3D9\",\"Изкуство\":\"#FFEBD6\",\"Музика\":\"#F2E6FF\",\"Компютърни науки\":\"#E3EAF5\",\"Икономика\":\"#E6F7F1\",\"Философия\":\"#ECE9F4\",\"Литература\":\"#FFF4E1\",\"Няма\":\"#F2F2F2\",\"Всякакви\":\"#F2F2F2\"}", response.body());
     }
+
+    @Test
+    void invalidImage(){
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:4567/"+"api/image/somethingThatDoesNotExist"))
+                .build();
+
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals(404, response.statusCode());
+    }
+
 
     void test(String url, int code, String message){
         HttpClient client = HttpClient.newHttpClient();
@@ -65,10 +81,5 @@ public class HTTPTest {
         }
 
         assertEquals(code, response.statusCode(), message);
-    }
-
-    @AfterAll
-    void tearDown() {
-        Spark.stop();
     }
 }
